e25a391ea6934d48cd412a264d3184ae171c59bb
Cristiano Notarangelo <cristianonota99@gmail.com>
2020-01-02 17:30:22 +0100

    First Commit

---
+++ esame-1/src/main/java/org/fondazionejac/architetturasw/Esame1Application.java
@@ -0,0 +1,13 @@
+package org.fondazionejac.architetturasw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esame1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esame1Application.class, args);
+	}
+
+}
---
+++ esame-1/src/test/java/org/fondazionejac/architetturasw/Esame1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fondazionejac.architetturasw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esame1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
